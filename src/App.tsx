254e4d2449da03a029d5b609b5f2643456923400
Dominico Yoon <808dominico@gmail.com>
2024-05-26 23:32:01 +0900

    2024.05.26 23:32 Commit 컴포넌트 이름 변경 후 App 랜더링 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
-import EnableTooltip from "./components/EnableTooltip";
-import DirectionTooltip from "./components/DirectionTooltip";
-import StyleTooltip from "./components/StyleTooltip";
-import DelayTooltip from "./components/DelayTooltip";
+import TooltipAvailable from "./components/TooltipAvailable";
+import TooltipDirection from "./components/TooltipDirection";
+import TooltipStyle from "./components/TooltipStyle";
+import TooltipDelay from "./components/TooltipDelay";
 
 const App = () => {
   return (
     <div className="container">
       <div className="container-layout">
         <section style={{ height: "100vh", padding: "0px 200px" }}>
-          <DirectionTooltip />
-          <DelayTooltip />
-          <StyleTooltip />
-          <EnableTooltip />
+          <TooltipDirection />
+          <TooltipDelay />
+          <TooltipStyle />
+          <TooltipAvailable />
         </section>
       </div>
     </div>
